db1af5386c569995dbf332b6982c9e39771994b2
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-04-13 19:52:26 -0300

    feat(fonts): add fonts token

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export * as colors from './colors';
-export * as fonts from './fonts';
+
+export * from './fonts';
+export type { Primary, Secondary } from './fonts';
+
 export * as gradients from './gradients';
 export * as sizes from './sizes';
 export * as spaces from './spaces';
