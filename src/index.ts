67d2f20b16f34067a47e7a9f705cec373a93377a
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-04-13 19:53:18 -0300

    feat(sizes): add sizes token

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export * as colors from './colors';
 export * as fonts from './fonts';
 export * as gradients from './gradients';
-export * as sizes from './sizes';
+
+export * from './sizes';
+export type { Size } from './sizes';
+
 export * as spaces from './spaces';
--- src/sizes/index.ts
+++ src/sizes/index.ts
@@ -1,1 +1,11 @@
-export {};
+export const sizes = {
+  verySmall: 'very-small',
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+} as const;
+
+type SizesKeys = keyof typeof sizes;
+type Size = typeof sizes[SizesKeys];
+
+export type { Size };
