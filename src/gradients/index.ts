0cde2ffebbf5348bb047bd5556e724b46c631702
Murillo de Miranda Pereira <murillomir0@gmail.com>
2021-04-13 19:51:12 -0300

    feat(gradients): add gradients token

--- src/gradients/index.ts
+++ src/gradients/index.ts
@@ -1,1 +1,53 @@
-export {};
+import { colors } from '../colors';
+import type { Color } from '../colors';
+
+const {
+  earth200,
+  earth500,
+  uranus400,
+  uranus500,
+  uranus600,
+  moon400,
+  moon700,
+  moon1000,
+  venus400,
+  venus700,
+  mars300,
+  mars400,
+} = colors;
+
+type GradientColors = Color | '#fff56a' | '#ffd265' | '#bac8d1' | '#d991e0' | '#00c6d4' | '#00ea60';
+
+type GradientColor = [GradientColors, GradientColors] | [GradientColors, GradientColors, GradientColors];
+
+type Gradient = {
+  sombrero: GradientColor;
+  hoag: GradientColor;
+  pinwheel: GradientColor;
+  blackHole: GradientColor;
+  milkyWay: GradientColor;
+  whirlpool: GradientColor;
+  nebulosa: GradientColor;
+  andromeda: GradientColor;
+  centaurus: GradientColor;
+  cartwheel: GradientColor;
+  mayall: GradientColor;
+};
+
+type GradientID = keyof Gradient;
+
+export const gradients: Gradient = {
+  sombrero: [uranus400, earth200],
+  hoag: ['#fff56a', '#ffd265'],
+  pinwheel: [moon400, '#bac8d1'],
+  blackHole: [moon700, moon1000],
+  milkyWay: [uranus500, venus400],
+  whirlpool: [earth200, earth500],
+  nebulosa: ['#d991e0', '#00c6d4', '#00ea60'],
+  andromeda: [mars300, venus400],
+  centaurus: [venus400, venus700],
+  cartwheel: [uranus600, uranus400],
+  mayall: [mars300, mars400],
+};
+
+export type { Gradient, GradientColor, GradientID };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ export * from './colors';
 export type { Color } from './colors';
 
 export * as fonts from './fonts';
-export * as gradients from './gradients';
+
+export * from './gradients';
+export type { Gradient, GradientColor, GradientID } from './gradients';
+
 export * as sizes from './sizes';
 export * as spaces from './spaces';
