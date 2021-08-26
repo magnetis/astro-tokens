c020e155006eeb0491bada22a9385cc8750c3663
Luciano Lima <luciano.lima@magnetis.com.br>
2021-08-26 06:26:28 -0300

    chore: improve typescript config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.ts?(x)': (filenames) => {
     const files = filenames.join(' ');
 
-    return [`prettier --write ${files}`, `eslint --fix ${files}`, `tsc -p tsconfig.json --noEmit`];
+    return [`prettier --write ${files}`, `eslint --fix ${files}`, `tsc -p tsconfig.json`];
   },
   '**/*.js?(x)': (filenames) => {
     const files = filenames.join(' ');
